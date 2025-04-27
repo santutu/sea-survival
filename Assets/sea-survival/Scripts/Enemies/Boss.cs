3fab595e00a9c2bece977605d87806789de721b5
santutu <santutu@naver.com>
2025-04-27 19:13:00 +0900

    boss

---
+++ Assets/sea-survival/Scripts/Enemies/Boss.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace sea_survival.Scripts.Enemies
+{
+    [RequireComponent(typeof(Enemy))]
+    public class Boss : MonoBehaviour
+    {
+        private void Update()
+        {
+            //todo: 보스 기술 구현
+        }
+    }
+}
